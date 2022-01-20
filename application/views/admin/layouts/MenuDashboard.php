600fd7b8d861258cf0aa77fd09a3fdf13d7f3d2b
Frand AL <el_codee.elfrand3@gmail.com>
2022-01-21 00:10:10 +0700

    Aplikasi pegawai

---
+++ application/views/admin/layouts/MenuDashboard.php
@@ -0,0 +1,60 @@
+                    <div class="row">
+                    	<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+                    		<div class="card bg-dark influencer-profile-data">
+                    			<div class="card-body">
+                    				<div class="row">
+                    					<div class="col-xl-2 col-lg-4 col-md-4 col-sm-4 col-12">
+                    						<div class="text-center">
+                    							<img src="<?= base_url(); ?>assets/images/logos.png" alt="User Avatar"
+                    								class="rounded-circle user-avatar-xxl">
+                    						</div>
+                    					</div>
+                    					<div class="col-xl-10 col-lg-8 col-md-8 col-sm-8 col-12">
+                    						<div class="user-avatar-info">
+                    							<div class="m-b-20">
+                    								<div class="user-avatar-name">
+                    									<h2 class="mb-1 text-white"><?= PT_NAME; ?></h2>
+                    								</div>
+                    							</div>
+                    							<div class="user-avatar-address">
+                    								<p class="border-bottom pb-3">
+                    									<span class="d-xl-inline-block d-block mb-2"><i
+                    											class="fa fa-map-marker-alt mr-2 text-primary "></i>Jl.
+                    										Adisucipto Desa Teluk Kapuas Kec Sungai Raya Kabupaten Kubu Raya</span>
+                    								</p>
+                    								<div class="mt-3">
+														<p>Pada kesempatan kali imi saya mau membagikan sebuah source code PHP dengan CodeIgniter tentang manajemen absensi pegawai berbasis web secara gratis. Aplikasi ini dibangun dengan bahasa pemrograman PHP versi 7 penjelasan mengenai bahasa PHP  silahkan ikuti tautan berikut Mengenal PHP. Aplikasi ini dibuat dengan framework CodeIgniter (CI), CI sendiri memiliki konsep pengembangan MVC (Model, Views, Controller). </p>
+                    								</div>
+                    							</div>
+                    						</div>
+                    					</div>
+                    				</div>
+                    			</div>
+                    		</div>
+                    	</div>
+                    </div>
+                    <!-- ============================================================== -->
+                    <!-- end content  -->
+                    <!-- ============================================================== -->
+                    <div class="row">
+                    	<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+                    		<div class="row">
+                    			<div class="col-6 col-lg-6 col-md-6">
+                    				<div class="card bg-dark influencer-profile-data">
+                    					<div class="card-body">
+                    						<p>Aplikasi manajemen absensi pegawai ini merupakan pengembangan dan telah terdapat beberapa perubahan baik dari segi interface maupun fungsi di dalamnya. Aplikasi ini memiliki dua level hak akses yaitu administrator dan pegawai. Untuk administrator memiliki beberapa fasilitas seperti mengelola data pegawai, mengelola kehadiran pegawai, dan mengatur waktu absensi. Sedangkan untuk pegawai memiliki akses untuk mengisi absensi, melihat kehadiran, dan mengatur informasi akun.!</p>
+                    					</div>
+                    				</div>
+                    			</div>
+								<div class="col-6 col-lg-6 col-md-6">
+                    				<div class="card bg-dark influencer-profile-data">
+                    					<div class="card-body">
+                    						<p>Pertama buat database di XAMPP atau dengan yang lain setelah itu lakukan import file bg_pegawai.sql yang berada di folder absensipegawai\database ke dalam database selanjutnya sesuaikan konfigurasi koneksi database di file database.php yang berada di folder absensipegawai\application\config.!</p>
+                    					</div>
+                    				</div>
+                    			</div>
+                    		</div>
+                    	</div>
+                    </div>
+                    </div>
+                    </div>
